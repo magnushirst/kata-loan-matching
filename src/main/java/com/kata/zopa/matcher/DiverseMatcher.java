7c9399a16cc3ca98ce3948017cb6f62fdb4bef92
Magnus Hirst <magnus.hirst@uk.tesco.com>
2018-11-05 00:03:04 +0000

    Adding new implementation of matcher to add diversity for lenders

---
+++ src/main/java/com/kata/zopa/matcher/DiverseMatcher.java
@@ -0,0 +1,37 @@
+package com.kata.zopa.matcher;
+
+import com.kata.zopa.dto.Lender;
+import com.kata.zopa.dto.LoanAllocation;
+import com.kata.zopa.dto.LoanDetails;
+import com.kata.zopa.exception.CannotFulFillLoanRequest;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+
+/**
+ * Naive implementation of the matcher which provides the lowest interest for the borrower but with the
+ * most risk for the lenders. This could match 1 borrower to 1 lender.
+ */
+
+public class DiverseMatcher implements Matcher {
+    @Override
+    public LoanDetails match(int loanRequest, ArrayList<Lender> lenders) throws CannotFulFillLoanRequest {
+        BigDecimal splitLendingAmount = new BigDecimal(loanRequest).divide(new BigDecimal(lenders.size()), 2, RoundingMode.HALF_EVEN);
+
+        LoanAllocation loanAllocation = new LoanAllocation();
+        for (Lender lender : lenders) {
+            if (lender.getAvailable().compareTo(splitLendingAmount) >= 0) {
+                loanAllocation.addLender(lender.getRate(), splitLendingAmount);
+            }
+        }
+        return new LoanDetails(loanAllocation.getRate(), loanAllocation.getLoanedAmount());
+    }
+
+    private boolean hasLoanBeenFulfilled(BigDecimal loanRequest, LoanAllocation loanAllocation) {
+        return loanAllocation.loanedAmount()
+                .setScale(0, RoundingMode.FLOOR)
+                .compareTo(loanRequest) == 0;
+    }
+}
---
+++ src/test/java/com/kata/zopa/matcher/DiverseMatcherTest.java
@@ -0,0 +1,64 @@
+package com.kata.zopa.matcher;
+
+import com.kata.zopa.dto.Lender;
+import com.kata.zopa.dto.LoanDetails;
+import com.kata.zopa.exception.CannotFulFillLoanRequest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DiverseMatcherTest {
+    private DiverseMatcher diverseMatcher;
+
+    @Before
+    public void setUp() {
+        diverseMatcher = new DiverseMatcher();
+    }
+
+    @Test
+    public void whenCalledWithMultipleLendersWithSameRate_ThenMatchWholeLoanWithRate() throws CannotFulFillLoanRequest {
+        Lender lender1 = new Lender("Sarah", "0.1", "100");
+        Lender lender2 = new Lender("Bob", "0.1", "100");
+        ArrayList<Lender> lenders = new ArrayList<>(Arrays.asList(lender1, lender2));
+
+        LoanDetails loanDetails = diverseMatcher.match(100, lenders);
+        Assert.assertEquals(new BigDecimal("10.0"), loanDetails.getYearlyRate());
+        Assert.assertEquals(new BigDecimal("100.00"), loanDetails.getLoanAmount());
+    }
+
+    @Test
+    public void whenCalledWithMultipleLendersWithDifferentRates_ThenMatchWithAverageRate() throws CannotFulFillLoanRequest {
+        Lender lender1 = new Lender("Sarah", "0.01", "100");
+        Lender lender2 = new Lender("Bob", "0.02", "100");
+        Lender lender3 = new Lender("Sally", "0.03", "100");
+        Lender lender4 = new Lender("Jane", "0.04", "100");
+        Lender lender5 = new Lender("Dan", "0.05", "100");
+        ArrayList<Lender> lenders = new ArrayList<>(Arrays.asList(lender1, lender2, lender3, lender4, lender5));
+
+        LoanDetails loanDetails = diverseMatcher.match(100, lenders);
+        Assert.assertEquals(new BigDecimal("3.0"), loanDetails.getYearlyRate());
+        Assert.assertEquals(new BigDecimal("100.00"), loanDetails.getLoanAmount());
+    }
+
+    @Test
+    public void whenCalledWithLendersThatHaveInsufficentFunds_ThenIgnoreThem() throws CannotFulFillLoanRequest {
+        Lender lender1 = new Lender("Sarah", "0.01", "10");
+        Lender lender2 = new Lender("Bob", "0.02", "100");
+        Lender lender3 = new Lender("Sally", "0.03", "100");
+        Lender lender4 = new Lender("Jane", "0.04", "100");
+
+        ArrayList<Lender> lenders = new ArrayList<>(Arrays.asList(lender1, lender2, lender3, lender4));
+
+        LoanDetails loanDetails = diverseMatcher.match(100, lenders);
+        Assert.assertEquals(new BigDecimal("3.0"), loanDetails.getYearlyRate());
+        Assert.assertEquals(new BigDecimal("100.00"), loanDetails.getLoanAmount());
+    }
+
+
+
+
+}
